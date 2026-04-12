2e29e40f52985194d7dcdcedf05e30c6b8835adb
Muhammad Moinur Rahman <bofh@FreeBSD.org>
2026-04-12 22:43:29 +0200

    net-mgmt/py-aggregate6: Update version 1.0.12=>1.0.14
    Changelog: https://github.com/job/aggregate6/releases/tag/1.0.14

--- net-mgmt/py-aggregate6/files/patch-setup.py
+++ net-mgmt/py-aggregate6/files/patch-setup.py
@@ -1,10 +1,9 @@
---- setup.py.orig	2017-12-01 09:46:38 UTC
+--- setup.py.orig	2024-11-12 16:53:30 UTC
 +++ setup.py
-@@ -70,13 +70,12 @@ setup(
-         'Programming Language :: Python :: 3',
-         'Programming Language :: Python :: 3.6'
+@@ -77,12 +77,12 @@ setup(
+         'Programming Language :: Python :: 3.12'
      ],
--    setup_requires=["nose", "coverage", "mock"],
+     tests_require=["mock;python_version<'3.3'", "coverage"],
 -    install_requires=["py-radix==0.10.0"] + (
 +    install_requires=["py-radix>=0.10.0"] + (
          ["future", "ipaddress"] if sys.version_info.major == 2 else []
@@ -12,7 +11,7 @@
      packages=find_packages(exclude=['tests', 'tests.*']),
      entry_points={'console_scripts':
                    ['aggregate6 = aggregate6.aggregate6:main']},
--    data_files = [('man/man7', ['aggregate6.7'])],
-+    data_files = [('share/man/man7', ['aggregate6.7'])],
+-    data_files=[('man/man7', ['aggregate6.7'])],
++    data_files=[('share/man/man7', ['aggregate6.7'])],
      test_suite='nose.collector'
  )
